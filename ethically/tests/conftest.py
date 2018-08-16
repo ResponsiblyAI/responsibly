798990a11d13f54b6996d522199671a3131ccf67
shlomi_h <hod_s@bezeqint.net>
2018-08-16 22:50:49 +0200

    make matplotlib to pass

--- ethically/tests/conftest.py
+++ ethically/tests/conftest.py
@@ -1,6 +1,14 @@
 """Unit tests configuration file."""
 
+import os
+
 import log
+import matplotlib as mpl
+
+
+if os.environ.get('DISPLAY', '') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 
 
 def pytest_configure(config):
