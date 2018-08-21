7233c5a9b433cd7a718da99ea977ac5595881653
shlomi_h <hod_s@bezeqint.net>
2018-08-21 10:16:50 +0200

    sort data

--- ethically/we/data/__init__.py
+++ ethically/we/data/__init__.py
@@ -17,19 +17,22 @@ BOLUKBASI_DATA['gender']['profession_names'] = list(
     zip(*BOLUKBASI_DATA['gender']['professions']))[0]
 
 
+BOLUKBASI_DATA['gender']['specific_full'].sort()
+
 # TODO: in the code of the article, the last definitional pair
 # is not in the specific full
-BOLUKBASI_DATA['gender']['specific_full_with_definitional'] = (
+BOLUKBASI_DATA['gender']['specific_full_with_definitional'] = list(
     set.union(
         *map(set, BOLUKBASI_DATA['gender']['definitional_pairs'])
     ) | set(BOLUKBASI_DATA['gender']['specific_full'])
 )
-
+BOLUKBASI_DATA['gender']['specific_full_with_definitional'].sort()
 
 BOLUKBASI_DATA['gender']['neutral_profession_names'] = list(
     set(BOLUKBASI_DATA['gender']['profession_names'])
     - set(BOLUKBASI_DATA['gender']['specific_full_with_definitional'])
 )
+BOLUKBASI_DATA['gender']['neutral_profession_names'].sort()
 
 BOLUKBASI_DATA['gender']['word_group_keys'] = ['profession_names',
                                                'neutral_profession_names',
