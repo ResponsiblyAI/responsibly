73295b64d9c106a93e6e92fbb4e3847e63cc1441
shlomi_h <hod_s@bezeqint.net>
2019-03-08 12:52:12 +0100

    fixing error message

--- ethically/dataset/german/__init__.py
+++ ethically/dataset/german/__init__.py
@@ -93,7 +93,7 @@ class GermanDataset(Dataset):
     def _validate(self):
         # pylint: disable=line-too-long
         super()._validate()
-        assert len(self.df) == 1000, 'the number of rows should be 6172,'\
+        assert len(self.df) == 1000, 'the number of rows should be 1000,'\
                                      ' but it is {}.'.format(len(self.df))
         assert len(self.df.columns) == 23, 'the number of columns should be 23,'\
                                            ' but it is {}.'.format(len(self.df.columns))
