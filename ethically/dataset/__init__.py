6479ec5191b0ec880a12d1898d5f86c7d72cf918
shlomi_h <hod_s@bezeqint.net>
2019-03-08 12:15:39 +0100

    adding german dataset

--- ethically/dataset/__init__.py
+++ ethically/dataset/__init__.py
@@ -1,2 +1,3 @@
 from ethically.dataset.compas import COMPASDataset
 from ethically.dataset.core import Dataset
+from ethically.dataset.german import GermanDataset
--- ethically/tests/test_dataset.py
+++ ethically/tests/test_dataset.py
@@ -3,12 +3,17 @@
 
 import pytest
 
-from ethically.dataset import COMPASDataset
+from ethically.dataset import COMPASDataset, GermanDataset
 
 
 @pytest.fixture
 def compas_ds():
     return COMPASDataset()
+
+
+@pytest.fixture
+def german_ds():
+    return GermanDataset()
 
 
 def test_compas_validate(compas_ds):
@@ -20,3 +25,14 @@ def test_compas_str(compas_ds):
             == '<ProPublica Recidivism/COMPAS.'
             ' 6172 rows, 55 columns'
             ' in which {race} are sensitive attributes>')
+
+
+def test_german_validate(german_ds):
+    german_ds._validate()   # pylint: disable=protected-access
+
+
+def test_german_str(german_ds):
+    assert (str(german_ds)
+            == '<German Credit Dataset.'
+            ' 1000 rows, 23 columns'
+            ' in which {age_factor} are sensitive attributes>')
