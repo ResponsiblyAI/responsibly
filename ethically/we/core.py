c756d3ed62bb3b9daa26d9c6bdaad7fd096f6483
shlomi_h <hod_s@bezeqint.net>
2019-03-12 15:29:22 +0100

    reordering imports by pep8

--- ethically/we/core.py
+++ ethically/we/core.py
@@ -8,9 +8,8 @@ from scipy.stats import spearmanr
 from sklearn.decomposition import PCA
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.svm import LinearSVC
-from tqdm import tqdm
-
 from tabulate import tabulate
+from tqdm import tqdm
 
 from ..consts import RANDOM_STATE
 from .benchmark import evaluate_words_embedding
