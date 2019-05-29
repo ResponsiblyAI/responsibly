dd0782a36d94e6ef5ff77364084ac7bff01aa0be
shlomi_h <hod_s@bezeqint.net>
2019-05-29 17:59:36 +0200

    add disable line-too-long for pylint

--- ethically/__init__.py
+++ ethically/__init__.py
@@ -1,5 +1,5 @@
 __project__ = 'ethically'
-__description__ = 'Toolkit for Auditing and Mitigating Bias and Fairness of Machine Learning Systems 🔎🤖🧰'
+__description__ = 'Toolkit for Auditing and Mitigating Bias and Fairness of Machine Learning Systems 🔎🤖🧰'  # pylint: disable=line-too-long
 __url__ = 'http://docs.ethically.ai'
 __version__ = '0.0.3'
 __author__ = 'Shlomi Hod'
