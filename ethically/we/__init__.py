392ae805f68e8dd8f4f4960cd6531262170e0ec1
shlomi_h <hod_s@bezeqint.net>
2019-05-30 23:25:47 +0200

    fix class name spelling

--- ethically/we/__init__.py
+++ ethically/we/__init__.py
@@ -33,7 +33,7 @@ the impact of debiasing performance.
 
 """
 
-from .bias import BiasWordsEmbedding, GenderBiasWE
+from .bias import BiasWordEmbedding, GenderBiasWE
 from .data import load_w2v_small
 from .utils import most_similar
 from .weat import (
