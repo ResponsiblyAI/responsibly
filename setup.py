12762170b18faa9fe17668fb1f2e82fbe413af7d
shlomi_h <hod_s@bezeqint.net>
2019-08-03 18:57:13 -0700

    set gensim version to 3.7.0 or compatible

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setuptools.setup(
         "seaborn >= 0.9",
         "scikit-learn >= 0.19",
         "smart_open < 1.8",
-        "gensim ~= 3.7",
+        "gensim ~= 3.7.0",
         "tabulate >= 0.8",
         "six >= 1.10",
         "click >= 6.0",
