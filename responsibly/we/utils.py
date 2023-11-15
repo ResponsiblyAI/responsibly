e2cba7ceb367382d71cadf3e8d017a8819d18acb
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2023-11-15 21:00:59 +0000

    Update utils.py

--- responsibly/we/utils.py
+++ responsibly/we/utils.py
@@ -19,7 +19,7 @@ from gensim.models import KeyedVectors
 WORD_EMBEDDING_MODEL_TYPES = (gensim.models.keyedvectors.KeyedVectors,
                               # gensim.models.keyedvectors.BaseKeyedVectors,
                               gensim.models.fasttext.FastText,
-                              gensim.models.word2vec.Word2Vec,
+                              gensim.models.word2vec.Word2Vec,)
                               # gensim.models.base_any2vec.BaseWordEmbeddingsModel,)  # pylint: disable=line-too-long
 def round_to_extreme(value, digits=2):
     place = 10**digits
