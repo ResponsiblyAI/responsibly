76f6f6bcc435a24fa9c7dfcf1c32e7937614e346
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2023-11-15 22:23:07 +0000

    Update utils.py

--- responsibly/we/utils.py
+++ responsibly/we/utils.py
@@ -204,7 +204,7 @@ def most_similar(model, positive=None, negative=None,
             mean.append(weight * word)
         else:
             mean.append(weight * model.word_vec(word, use_norm=True))
-            if word in model:
+            if word in model.key:
                 all_words.add(model.key_to_index[word])
 
     if not mean:
